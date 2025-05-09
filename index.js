b5ccd7e946779cd6109727ad0c934f770150bc16
AMGI <amgi@mac.home>
2025-05-09 21:00:05 +0200

    pushes lab

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 // Iteration 1: Names and Input
+let hacker1 = "John"; // driver's name
+console.log(`The driver's name is ${hacker1}`);
 
+let hacker2 = "Maria"; // navigator's name
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker2.length > hacker1.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
 
 // Iteration 3: Loops
+// 3.1 Print driver's name in capital letters with spaces
+let spacedName = "";
+for (let i = 0; i < hacker1.length; i++) {
+  spacedName += hacker1[i].toUpperCase();
+  if (i < hacker1.length - 1) spacedName += " ";
+}
+console.log(spacedName);
+
+// 3.2 Print navigator's name in reverse order
+let reversedName = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reversedName += hacker2[i];
+}
+console.log(reversedName);
+
+// 3.3 Lexicographic comparison
+if (hacker1.localeCompare(hacker2) < 0) {
+  console.log("The driver's name goes first.");
+} else if (hacker1.localeCompare(hacker2) > 0) {
+  console.log("Yo, the navigator goes first, definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
+
+// Bonus 1: Lorem ipsum word counter
+let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tell
